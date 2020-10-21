00c86256835e41827766fa50ed81f81c31f6c9f7
sstrinic <72578940+sstrinic@users.noreply.github.com>
2020-10-21 21:30:24 +0200

    Upload

---
+++ Zadatak1/Source.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Student
+{
+	char ime[15];
+	char prezime[15];
+	int bodovi;
+}student;
+
+int LineCounter();
+int FileToStruct(student*, int);
+int PrintFromStruct(student*, int);
+
+int main()
+{
+	int br;
+	br = LineCounter();
+
+	student* studenti;
+	studenti = (student*)malloc(br * sizeof(student));
+
+	FileToStruct(studenti,br);
+	PrintFromStruct(studenti,br);
+
+	free(studenti);
+	return 0;
+}
+
+int LineCounter()
+{
+	FILE* f;
+	int x = 1;
+
+	f = fopen("studenti.txt", "r");
+
+	if (f == NULL)
+	{
+			printf("Greska prilikom citanja datoteke!!!\n");
+			exit(1);
+	}
+
+	while (!feof(f))
+	{
+
+		if (getc(f) == '\n')
+			x++;
+	}
+
+	fclose(f);
+	return x;
+}
+
+int FileToStruct(student* studenti, int br)
+{
+	FILE* fp;
+	fp = fopen("studenti.txt", "r");
+
+	for (int i = 0; i < br; i++)
+	{
+		fscanf(fp, "%s %s %d\n", studenti->ime, studenti->prezime, &studenti->bodovi);
+		studenti++;
+	}
+
+	fclose(fp);
+	return 0;
+}
+
+int PrintFromStruct(student* studenti, int br)
+{
+	int rel;
+	for (int i = 0; i < br; i++)
+	{
+		rel = (((float)studenti->bodovi)/50)*100,0;
+		printf("\nIme: %s Prezime: %s Bodovi: %d Relativni bodovi: %d %%", studenti->ime, studenti->prezime, studenti->bodovi,rel);
+		studenti++;
+	}
+	return 0;
+}
