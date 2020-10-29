a0e9f52c5e0baabd5d1b7e42bda224338e79f97a
sstrinic <72578940+sstrinic@users.noreply.github.com>
2020-10-29 17:43:48 +0100

    Update1

--- Zadatak2/Source.c
+++ Zadatak2/Source.c
@@ -1,33 +1,4 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-
-struct Osoba;
-typedef struct Osoba* Pozicija;
-typedef struct Osoba _osoba;
-
-struct Osoba
-{
-	char ime[15];
-	char prezime[15];
-	int godina;
-	Pozicija next;
-};
-
-void Ispis(Pozicija);
-void UnosPocetak(Pozicija);
-void UnosKraj(Pozicija);
-Pozicija Trazi(char[], Pozicija);
-Pozicija TraziPrethodni(char[], Pozicija);
-void Brisi(Pozicija, char[]);
-void Meni(Pozicija, int);
-void CleanExit(Pozicija);
-void IspisMeni();
-void AddNewAfterElement(char[], Pozicija);
-void AddNewBeforeElement(char[], Pozicija);
-void ListToFile(Pozicija);
-void ReadFile();
-void SortListBySName(Pozicija);
+#include "Declarations.h"
 
 int main()
 {
